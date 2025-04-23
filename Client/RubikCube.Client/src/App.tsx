08e8d29d872b983aae982dc30ffdb81baa5292d3
Петър Нешков <petio567@abv.bg>
2025-04-24 02:47:21 +0300

    Implemented NotFound page and logic

--- Client/RubikCube.Client/src/App.tsx
+++ Client/RubikCube.Client/src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import CubeViewer from "./components/pages/cube/CubeViewer.tsx";
+import NotFound from './components/pages/not-found/NotFoundPage.tsx';
 
 const App = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<CubeViewer/>}/>
+                <Route path="*" element={<NotFound />} />
             </Routes>
         </BrowserRouter>
     );
---
+++ Client/RubikCube.Client/src/components/pages/not-found/NotFoundPage.tsx
@@ -0,0 +1,35 @@
+import image from './page-not-found.png';
+
+import styles from './NotFoundPage.module.css';
+import { useNavigate } from 'react-router-dom';
+import {useCallback, useEffect } from 'react';
+
+const NotFound = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        document.title = "Page not Found";
+
+        const timeout = setTimeout(() => {
+            navigate("/");
+        }, 5000);
+
+        return () => clearTimeout(timeout);
+    }, [navigate]);
+
+    const handleBackToHome = useCallback(() => {
+        navigate('/');
+    },[navigate]);
+    
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.heading}>404 - Page Not Found</h1>
+            <p className={styles.text}>Oops! The page you're looking for doesn't exist.</p>
+            <img className={styles.image} src={image} alt="Page Not Found" />
+            <button className={styles.homeButton} onClick={handleBackToHome}>
+                Back to Home
+            </button>
+        </div>
+    );
+};
+export default NotFound;
